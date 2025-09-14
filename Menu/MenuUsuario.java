fb22bc032871e6f8da925a38f31b2f4854b3fa55
Mateus Martins Parreiras <160194896+marvnnn@users.noreply.github.com>
2025-09-14 20:30:03 -0300

    updateSenha e ExcluirUsuario

--- Menu/MenuUsuario.java
+++ Menu/MenuUsuario.java
@@ -58,16 +58,6 @@ public class MenuUsuario {
         } else {
             System.out.println("Nome de usuário ou senha incorretos.");
         }
-        // System.out.println(u.getName());
-
-        // int id = arqUsu.readByCPF(cpf);
-        // Usuario u = arqUsu.read(id-1);
-        // if (u != null) {
-        // System.out.println("Login bem sucedido! Bem-vindo, " + u.getName());
-        // } else {
-        // System.out.println("Nome de usuário ou senha incorretos.");
-        // }
-        // System.out.println("Não foi possivel efetuar o login.");
     }
 
     public void registrar() {
@@ -76,7 +66,7 @@ public class MenuUsuario {
         //String cpf = console.nextLine();
         System.out.println("Digite o nome do usuário: ");
         String name = console.nextLine();
-        System.out.println("digite o email: ");
+        System.out.println("Digite o email: ");
         String email = console.nextLine();
         System.out.println("Digite a senha: ");
         String senha = console.nextLine();
@@ -96,7 +86,7 @@ public class MenuUsuario {
         }
     }
 
-    public void menuUsuario() {
+    public void menuUsuario() throws Exception{
         int opcao;
         console = new Scanner(System.in);
         do {
@@ -106,8 +96,11 @@ public class MenuUsuario {
 
             System.out.println("\n0 - Sair");
             System.out.println("1 - Adicionar Lista");
-            System.out.println("2 - Remover Lista");
-            System.out.println("3 - Ver Listas");
+            System.out.println("2 - Atualizar Lista");
+            System.out.println("3 - Remover Lista");
+            System.out.println("4 - Ver Listas");
+            System.out.println("5 - Alterar Senha");
+            System.out.println("6 - Deletar Usuário");
 
             System.out.print("\nOpção: ");
             opcao = console.nextInt();
@@ -121,9 +114,84 @@ public class MenuUsuario {
                     break;
                 case 3:
                     break;
+                case 4:
+                    break;
+                case 5: atualizarSenha();
+                    break;
+                case 6: deletarUsuario();
+                    break;
                 default:
                     break;
             }
-        } while (opcao != 0);
+        } while (opcao != 0 && opcao != 6);
+    }
+
+    public void atualizarSenha()  throws Exception{
+        console = new Scanner(System.in);
+        System.out.println("\n\nEasyGift 1.0");
+        System.out.println("---------");
+        System.out.println("> Alteração de Senha - Autenticação de Segurança");
+        System.out.println("\nDigite o email cadastrado na sua conta: ");
+        String email = console.nextLine();
+        System.out.println("Digite sua senha atual: ");
+        String senha = console.nextLine();
+
+        ParEmailID par = indiceEmail.read(email.hashCode());
+
+        Usuario u = arqUsu.read(par.getId());
+
+        if(u != null && u.verificarSenha(senha)) {
+            System.out.println("Autenticação feita com sucesso!");
+            System.out.println("\n\nDigite sua nova senha: ");
+            String newSenha = console.nextLine();
+            u.alterarSenha(newSenha);
+            boolean resultado = arqUsu.update(u);
+
+            if(resultado) {
+                System.out.println("Senha atualizada com sucesso!");
+            }
+            else {
+                System.out.println("Houve um problema ao atualizar sua senha, tente novamente.");
+            }
+        }
+        else {
+            System.out.println("E-mail ou senha incorretos.");
+        }
+
+    }
+
+    public void deletarUsuario() throws Exception{
+        console = new Scanner(System.in);
+        System.out.println("\n\nEasyGift 1.0");
+        System.out.println("---------");
+        System.out.println("> Exclusão de Usuário - Autenticação de Segurança");
+        System.out.println("\nDigite o email cadastrado na sua conta: ");
+        String email = console.nextLine();
+        System.out.println("Digite sua senha: ");
+        String senha = console.nextLine();
+
+        ParEmailID par = indiceEmail.read(email.hashCode());
+
+        Usuario u = arqUsu.read(par.getId());
+
+        if(u != null && u.verificarSenha(senha)) {
+            System.out.println("Verificação feita com sucesso!");
+            System.out.println("Você tem certeza que deseja excluir sua conta?(S/N)");
+            String resp = console.nextLine();
+            if(resp.equals("S")) {
+                boolean resposta = arqUsu.delete(par.getId());
+                if(resposta) {
+                    System.out.println("Conta excluída com sucesso!");
+                    return;
+                }
+                else {
+                    System.out.println("Houve um erro ao excluir sua conta, tente novamente mais tarde.");
+                }
+                
+            }
+            else {
+                return;
+            }
+        }
     }
 }
